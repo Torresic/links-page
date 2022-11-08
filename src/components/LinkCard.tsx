3ba1d9e86e07e0dc96efaa6544522cffd8fb3dca
Torresic <torresic09@icloud.com>
2022-11-08 14:50:00 +0100

    Add Links config file and UI

--- src/components/LinkCard.tsx
+++ src/components/LinkCard.tsx
@@ -1,21 +1,85 @@
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 import styled from "styled-components";
 
 interface LinkCardProps {
-    icon: string;
-    title: string;
-    color: string;
+  icon: string;
+  title: string;
+  color: string;
+  order: number;
 }
 
 export const LinkCard = (props: LinkCardProps) => {
-    const { icon, title, color } = props;
-    
-    return (
-        <CardContainer>
-        
-        </CardContainer>
-    )
+  const { icon, title, color, order } = props;
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+
+  return (
+    <CardContainer
+      color={color}
+      order={order}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <CardIcon src={icon} />
+      <CardTitle>{title}</CardTitle>
+      <CardIconOnHover isHovered={isHovered} icon={faArrowRight} />
+    </CardContainer>
+  );
+};
+
+interface CardContainerProps {
+  color: LinkCardProps["color"];
+  order: LinkCardProps["order"];
 }
 
-const CardContainer = styled.div`
-    width: 60%;
-`
+const CardContainer = styled.div<CardContainerProps>`
+  width: 80%;
+  height: 100px;
+  padding: ${(p) =>
+    `${p.theme.spacing.spacing12} ${p.theme.spacing.spacing48}`};
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  order: ${(p) => p.order};
+  cursor: pointer;
+
+  border: 1px solid transparent;
+  border-radius: ${(p) => p.theme.borderRadius.roundedXL};
+  transition: all 1 ease-in-out;
+  background: ${(
+    p
+  ) => `linear-gradient(0.25turn, ${p.color} ,${p.theme.colors.secondary} ) padding-box,
+    linear-gradient(${p.theme.colors.primary}, ${p.theme.colors.primaryTransparent}) border-box`};
+  box-shadow: 0px 4px 25px -10px ${(p) => p.theme.colors.primary};
+  &:hover {
+    transition: all 0.5s ease-in-out;
+    -webkit-box-shadow: inset 0px 0px 37px 3px rgba(0, 0, 0, 0.75);
+    -moz-box-shadow: inset 0px 0px 37px 3px rgba(0, 0, 0, 0.75);
+    box-shadow: 0px 4px 20px 10px ${(p) => p.theme.colors.primary},
+      inset 0px 0px 51px 24px rgba(0, 0, 0, 0.75);
+    }
+`;
+
+const CardIcon = styled.img``;
+
+const CardTitle = styled.h2`
+  color: ${(p) => p.theme.colors.white};
+  font-size: clamp(16px, 2vw, 32px);
+  line-height: clamp(16px, 2vw, 32px);
+  font-family: "Lato";
+  font-weight: 300;
+  text-align: center;
+`;
+
+interface CardIconHover {
+  isHovered: boolean;
+}
+
+const CardIconOnHover = styled(FontAwesomeIcon)<CardIconHover>`
+  visibility: ${(p) => (p.isHovered === false ? "hidden" : "visible")};
+  color: ${(p) => p.theme.colors.white};
+  font-size: clamp(16px, 2vw, 32px);
+`;
---
+++ src/links/Links.ts
@@ -0,0 +1,38 @@
+export const Links = [
+    {
+        icon: '/icons/youtube.png',
+        color: '#E01C1C',
+        title: 'YouTube',
+        order: 6,
+    },
+    {
+        icon: '/icons/github.png',
+        color: '#222222',
+        title: 'GitHub',
+        order: 4,
+    },
+    {
+        icon: '/icons/linkedin.png',
+        color: '#1E90C3',
+        title: 'LinkedIn',
+        order: 3,
+    },
+    {
+        icon: '/icons/twitter.png',
+        color: '#23C9E5',
+        title: 'Twitter',
+        order: 5,
+    },
+    {
+        icon: '/icons/hireme.png',
+        color: '#A66ABA',
+        title: 'Hire Me',
+        order: 2,
+    },
+    {
+        icon: '/icons/portfolio.png',
+        color: '#4141F9',
+        title: 'My Website',
+        order: 1,
+    },
+]
